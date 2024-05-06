9e0e058d3a83bce75e5052806e6c60dacad6b05e
qsqcqs <118565709+qsqcqs@users.noreply.github.com>
2024-05-05 23:17:13 -0700

    Add files via upload

---
+++ arch-dotnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /tmp
+mkdir dotnet-sdk-fun
+cd dotnet-sdk-fun
+wget https://archlinux.org/packages/extra/x86_64/dotnet-runtime/download/
+mv index.html uwu
+wget https://archlinux.org/packages/extra/x86_64/dotnet-sdk/download/
+mv index.html owo
+wget https://archlinux.org/packages/extra/x86_64/dotnet-targeting-pack/download/
+mv index.html qwq
+wget https://archlinux.org/packages/extra/x86_64/netstandard-targeting-pack/download/
+sudo pacman -U uwu owo qwq index.html
